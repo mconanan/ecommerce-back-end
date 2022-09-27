89b2edd43f2aeac62d73a70f59c1f438cffd6d66
madeleine <mconanan@gmail.com>
2022-09-26 18:20:39 -0700

    working code; need to record video and link screencastify in readme

---
+++ README.md
@@ -0,0 +1,12 @@
+MODULE 13 CHALLENGE; BUILD AN ECOMMERCE BACK END
+
+The back-end for an E-Commerce site was built so that the items in the database could be manipulated in Insomnia.
+
+The Express library was used to handle the server, and Mysql2 and Sequelize were used to create the database.
+
+The Dotenv package was used so sensitve information did not need to be hardcoded into the server, or uploaded to GitHub.
+
+As there is no front-end to this project, all routes were tested through insomnia.
+
+Link to repo: https://github.com/mconanan/ecommerce-back-end
+Link to screencastify: 
--- models/index.js
+++ models/index.js
@@ -5,12 +5,41 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+
+  foreignKey: 'product_id',
+  // Define an alias for when data is retrieved
+  as: 'products_tagged'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+
+  foreignKey: 'product_id',
+  // Define an alias for when data is retrieved
+  as: 'tagged_products'
+});
+
 
 module.exports = {
   Product,
